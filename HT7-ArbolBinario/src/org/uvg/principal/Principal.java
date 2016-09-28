5d974090ed2b9ec06467c685c6e4600f63560122
davisalvarez <davisen321@gmail.com>
2016-09-27 23:04:44 -0600

    Add files via upload

--- HT7-ArbolBinario/src/org/uvg/principal/Principal.java
+++ HT7-ArbolBinario/src/org/uvg/principal/Principal.java
@@ -1,5 +1,16 @@
 package org.uvg.principal;
 
+/*
+ * UVG
+ * Algoritmos y Estructura de Datos
+ * HT 7 - Arboles Binarios
+ * Autores:
+ * 		Davis Alvarez -15842
+ * 		Osiris Fuentes - 15240
+ * 
+ * GIT: https://github.com/davisalvarez/HT7-ArbolBinario.git
+*/		 
+
 public class Principal {
 
 	public static void main(String[] args) {
